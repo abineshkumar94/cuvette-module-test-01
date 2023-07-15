5ea5bb24a6f431d22c40073b2ea9ed11b7a95e06
abineshkumar94 <abi1205.ak@gmail.com>
2023-07-15 18:43:47 +0530

    game-play-improved

--- app.js
+++ app.js
@@ -1,85 +1,123 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const btnrules = document.querySelector('.rules-btn');
-    const btnclose = document.querySelector('.close-btn');
-    const rulemodel = document.querySelector('.rule-model');
-    const choicebuttons = document.querySelectorAll('.choice-btn');
-    const gamediv = document.querySelector('.game');
-    const resultsdiv = document.querySelector('.results');
-    const resultdivs = document.querySelectorAll('.results_result');
+  const btnrules = document.querySelector('.rules-btn');
+  const btnclose = document.querySelector('.close-btn');
+  const rulemodel = document.querySelector('.rule-model');
+  const choicebuttons = document.querySelectorAll('.choice-btn');
+  const resultsdiv = document.querySelector('.results');
+  const resultHeadings = document.querySelectorAll('.results_heading');
+  const resultdivs = document.querySelectorAll('.results_result');
+  const lines = document.querySelectorAll('.line1, .line2, .line3');
+  const resultwinner = document.querySelector(".results_winner");
+  const resulttext = document.querySelectorAll(".results_text, .ag-pc");  
+  
+  const CHOICES = [
+    {
+      name: "paper",
+      beats: "rock"
+    },
+    {
+      name: "scissors",
+      beats: "paper"
+    },
+    {
+      name: "rock",
+      beats: "scissors"
+    },
+  ];
 
-    const CHOICES = [
-        {
-            name: "paper",
-            beats: "rock"
-        },
-        {
-            name: "scissors",
-            beats: "paper"
-        },
-        {
-            name: "rock",
-            beats: "scissors"
-        },
-    ];
+  choicebuttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const choicename = button.dataset.choice;
+      const choice = CHOICES.find(choice => choice.name === choicename);
+      choose(choice);
+    });
+  });
 
+  function choose(choice) {
+    const pcpick = pcchoose();
+    displayResults([choice, pcpick]);
+    displaywinner([choice, pcpick]);
+    hideChoiceButtons();
+    hideLines();
+  }
+
+  function pcchoose() {
+    const random = Math.floor(Math.random() * CHOICES.length);
+    return CHOICES[random];
+  }
+
+  function displayResults(results) {
+    resultHeadings[0].textContent = "YOU PICKED";
+    resultHeadings[1].textContent = "PC PICKED";
+
+    resultdivs[0].innerHTML = `
+      <div class="choice ${results[0].name}">
+        <img src="${getImagePath(results[0].name)}" alt="${results[0].name}" />
+      </div>
+    `;
+
+    resultdivs[1].innerHTML = `
+      <div class="choice ${results[1].name}">
+        <img src="${getImagePath(results[1].name)}" alt="${results[1].name}" />
+      </div>
+    `;
+
+    resultsdiv.classList.remove('hidden');
+  }
+
+  function hideChoiceButtons() {
     choicebuttons.forEach(button => {
-        button.addEventListener('click', () => {
-            const choicename = button.dataset.choice;
-            const choice = CHOICES.find(choice => choice.name === choicename);
-            choose(choice);
-        });
+      button.style.display = 'none';
     });
+  }
 
-    function choose(choice) {
-        const pcpick = pcchoose();
-        displayresults([choice, pcpick]);
+  function hideLines() {
+    lines.forEach(line => {
+      line.style.display = 'none';
+    });
+  }
+
+  function getImagePath(choiceName) {
+    const imgElements = document.querySelectorAll('.choice img');
+    for (let i = 0; i < imgElements.length; i++) {
+      const imgElement = imgElements[i];
+      if (imgElement.alt.toLowerCase().includes(choiceName.toLowerCase())) {
+        return imgElement.src;
+      }
     }
+    return '';
+  }
 
-    function pcchoose() {
-        const random = Math.floor(Math.random() * CHOICES.length);
-        return CHOICES[random];
-    }
+  function displaywinner(results) {
+    setTimeout(() => {
+      const userwins = iswinner(results);
+      const pcwins = iswinner(results.reverse());
 
-    function displayresults(results) {
-        let counter = 0;
-        const interval = setInterval(() => {
-            if (counter >= resultdivs.length) {
-                clearInterval(interval);
-                gamediv.classList.toggle('hidden');
-                resultsdiv.classList.toggle('hidden');
-                return;
-    
-   }
+      if (userwins) {
+        resulttext[0].innerText = "YOU WIN";
+      } else if (pcwins) {
+        resulttext[0].innerText = "YOU LOST";
+      } else {
+        resulttext[0].innerText = "TIE UP";
+      }
+    }, 1000); 
 
-    function getImagePath(choiceName) {
-        const imgElements = document.querySelectorAll('.choice img');
-            for (let i = 0; i < imgElements.length; i++) {
-                const imgElement = imgElements[i];
-            if (imgElement.alt.toLowerCase().includes(choiceName.toLowerCase())) {
-                return imgElement.src;
-                }
-            }
-                return '';
-    }
-
-            resultdivs[counter].innerHTML = `
-                <div class="choice ${results[counter].name}">
-                    <img src="${getImagePath(results[counter].name)}" alt="${results[counter].name}" />
-                </div>
-            `;
-            counter++;
-        }, 1000);
-    }
-
-
-
+    resultwinner.classList.toggle('hidden');
+    resultsdiv.classList.toggle('show-winner');
+  }
  
-    
-    btnrules.addEventListener('click', () => {
-        rulemodel.style.display = 'block';
-    });
+  function iswinner(results) {
+    return results[0].beats == results[1].name;
+  }
 
-    btnclose.addEventListener('click', () => {
-        rulemodel.style.display = 'none';
-    });
+  btnrules.addEventListener('click', () => {
+    rulemodel.style.display = 'block';
+  });
+
+  btnclose.addEventListener('click', () => {
+    rulemodel.style.display = 'none';
+  });
 });
+
+  
+  
