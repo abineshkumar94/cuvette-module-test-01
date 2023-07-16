86570bc6656c2ec12dc90eb3ff601e3a41d3d31e
abineshkumar94 <abi1205.ak@gmail.com>
2023-07-16 10:55:18 +0530

    optimised-gameplay

--- app.js
+++ app.js
@@ -7,21 +7,26 @@ document.addEventListener('DOMContentLoaded', () => {
   const resultHeadings = document.querySelectorAll('.results_heading');
   const resultdivs = document.querySelectorAll('.results_result');
   const lines = document.querySelectorAll('.line1, .line2, .line3');
-  const resultwinner = document.querySelector(".results_winner");
-  const resulttext = document.querySelectorAll(".results_text, .ag-pc");  
-  
+  const resultwinner = document.querySelector('.results_winner');
+  const resulttext = document.querySelectorAll('.results_text, .ag-pc');
+  const playagainbtn = document.querySelector('.play-again');
+  const game = document.querySelector('.game');
+  const scoreNumber = document.querySelector('.score-number');
+  const comScoreNumber = document.querySelector('.com-score-number');
+  let score = 0;
+
   const CHOICES = [
     {
-      name: "paper",
-      beats: "rock"
+      name: 'paper',
+      beats: 'rock'
     },
     {
-      name: "scissors",
-      beats: "paper"
+      name: 'scissors',
+      beats: 'paper'
     },
     {
-      name: "rock",
-      beats: "scissors"
+      name: 'rock',
+      beats: 'scissors'
     },
   ];
 
@@ -47,8 +52,8 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function displayResults(results) {
-    resultHeadings[0].textContent = "YOU PICKED";
-    resultHeadings[1].textContent = "PC PICKED";
+    resultHeadings[0].textContent = 'YOU PICKED';
+    resultHeadings[1].textContent = 'PC PICKED';
 
     resultdivs[0].innerHTML = `
       <div class="choice ${results[0].name}">
@@ -61,7 +66,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <img src="${getImagePath(results[1].name)}" alt="${results[1].name}" />
       </div>
     `;
-
+    
     resultsdiv.classList.remove('hidden');
   }
 
@@ -92,25 +97,49 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
       const userwins = iswinner(results);
       const pcwins = iswinner(results.reverse());
-
+  
       if (userwins) {
-        resulttext[0].innerText = "YOU WIN";
-        resultdivs[0].classList.toggle('winner')
+        resulttext[0].innerText = 'YOU WIN';
+        resultdivs[0].classList.toggle('winner');
+        score++; // Increment the user's score
       } else if (pcwins) {
-        resulttext[0].innerText = "YOU LOST";
-        resultdivs[1].classList.toggle('winner')
+        resulttext[0].innerText = 'YOU LOST';
+        resultdivs[1].classList.toggle('winner');
+        // Increment the PC's score
+        // Remove the score increment for the user (score++;)
       } else {
-        resulttext[0].innerText = "TIE UP";
+        resulttext[0].innerText = 'TIE UP';
+        // It's a tie, no need to update the score
       }
-    }, 1000); 
-
+  
+      // Update the scores in the DOM
+      scoreNumber.textContent = userwins ? score : score - 1; // Update only the user's score
+      comScoreNumber.textContent = pcwins ? score : score - 1; // Update only the PC's score
+    }, 1000);
+  
     resultwinner.classList.toggle('hidden');
     resultsdiv.classList.toggle('show-winner');
   }
- 
+  
+
   function iswinner(results) {
     return results[0].beats == results[1].name;
   }
+
+  playagainbtn.addEventListener('click', () => {
+    // Hide the results
+    resultsdiv.classList.add('hidden');
+    resultwinner.classList.add('hidden');
+    // Show the choice buttons
+    choicebuttons.forEach(button => {
+      button.style.display = 'block';
+    });
+    // Show the lines
+    lines.forEach(line => {
+      line.style.display = 'block';
+    });
+  });
+  
 
   btnrules.addEventListener('click', () => {
     rulemodel.style.display = 'block';
@@ -120,6 +149,11 @@ document.addEventListener('DOMContentLoaded', () => {
     rulemodel.style.display = 'none';
   });
 });
+
+
+
+
+
 
   
   
